d5e9f79625e96c6142b5b2f453d8efe50dfbf07a
asukim <asukim2020@gmail.com>
2021-08-16 13:34:35 +0900

    스프링 데이터 and Querydsl 페이징 처리

--- querydsl/querydsl/src/main/java/study/querydsl/controller/MemberController.java
+++ querydsl/querydsl/src/main/java/study/querydsl/controller/MemberController.java
@@ -1,11 +1,14 @@
 package study.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
+import study.querydsl.repository.MemberRepository;
 
 import java.util.List;
 
@@ -14,10 +17,24 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     // localhost:8080/v1/members?teamName=teamB&ageGoe=31&ageLoe=35&username=member31
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    // localhost:8080/v2/members?page=0&size=5
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    // localhost:8080/v3/members?page=0&size=5
+    // localhost:8080/v3/members?page=0&size=200 -> 카운트 쿼리 안나감
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
